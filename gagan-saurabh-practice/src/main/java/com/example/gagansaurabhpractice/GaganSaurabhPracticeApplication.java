8df826a70083e7fea34c0ffc6a84ce6730ca6771
saurabhsisodia <saurabh.sisodia@vuclip.com>
2022-11-07 01:33:40 +0530

    adding project

---
+++ gagan-saurabh-practice/src/main/java/com/example/gagansaurabhpractice/GaganSaurabhPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gagansaurabhpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaganSaurabhPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaganSaurabhPracticeApplication.class, args);
+	}
+
+}
---
+++ gagan-saurabh-practice/src/test/java/com/example/gagansaurabhpractice/GaganSaurabhPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gagansaurabhpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaganSaurabhPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
